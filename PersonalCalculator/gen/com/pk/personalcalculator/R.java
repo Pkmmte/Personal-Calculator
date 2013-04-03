f744a64b681f7026e3ddc6e6451e7fd3a38bcc86
Pkmmte <pkx7225@gmail.com>
2013-04-02 19:09:48 -0700

    Slight app icon changes to make it stand out better in white background. (Very slight coloring, thicker border, and darker letters)
    I also fixed an issue where you would be brought to a blank page after
    pressing the back button.

--- PersonalCalculator/gen/com/pk/personalcalculator/R.java
+++ PersonalCalculator/gen/com/pk/personalcalculator/R.java
@@ -25,19 +25,20 @@ public final class R {
         public static final int cyan=0x7f050000;
         public static final int cyan2=0x7f050001;
         public static final int cyan3=0x7f050002;
-        public static final int divider=0x7f050007;
+        public static final int divider=0x7f050008;
         public static final int gray_dark=0x7f050006;
+        public static final int gray_darker=0x7f050007;
         public static final int gray_light=0x7f050005;
-        public static final int holo_blue_bright=0x7f050011;
-        public static final int holo_blue_dark=0x7f050009;
-        public static final int holo_blue_light=0x7f050008;
-        public static final int holo_green_dark=0x7f05000b;
-        public static final int holo_green_light=0x7f05000a;
-        public static final int holo_orange_dark=0x7f050010;
-        public static final int holo_orange_light=0x7f05000f;
-        public static final int holo_purple=0x7f05000e;
-        public static final int holo_red_dark=0x7f05000d;
-        public static final int holo_red_light=0x7f05000c;
+        public static final int holo_blue_bright=0x7f050012;
+        public static final int holo_blue_dark=0x7f05000a;
+        public static final int holo_blue_light=0x7f050009;
+        public static final int holo_green_dark=0x7f05000c;
+        public static final int holo_green_light=0x7f05000b;
+        public static final int holo_orange_dark=0x7f050011;
+        public static final int holo_orange_light=0x7f050010;
+        public static final int holo_purple=0x7f05000f;
+        public static final int holo_red_dark=0x7f05000e;
+        public static final int holo_red_light=0x7f05000d;
         public static final int white=0x7f050003;
     }
     public static final class dimen {
